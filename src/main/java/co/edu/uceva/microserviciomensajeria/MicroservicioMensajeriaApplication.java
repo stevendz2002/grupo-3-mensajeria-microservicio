a824102a70b9c6ad47946b62644b90892b13e14e
Steven Loaiza diaz <jhon.loaiza01@uceva.edu.co>
2025-03-31 20:03:04 -0500

    primer commit de creación  microservicio-mensajeria

---
+++ src/main/java/co/edu/uceva/microserviciomensajeria/MicroservicioMensajeriaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.microserviciomensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMensajeriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioMensajeriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/microserviciomensajeria/MicroservicioMensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.microserviciomensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMensajeriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
