fbb35b63b50e9b663e50bc8776c948ffa309e597
Steven Loaiza diaz <jhon.loaiza01@uceva.edu.co>
2025-03-31 21:07:10 -0500

    se agrega MensajeriaRestController.java

---
+++ src/main/java/co/edu/uceva/microserviciomensajeria/controllers/MensajeriaRestController.java
@@ -0,0 +1,54 @@
+package co.edu.uceva.microserviciomensajeria.controllers;
+
+import co.edu.uceva.microserviciomensajeria.model.entities.Mensajeria ;
+import co.edu.uceva.microserviciomensajeria.model.services.IMensajeriaService ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/mensajeria-service")
+public class MensajeriaRestController {
+
+    // Inyección de dependencia del servicio que proporciona servicios de CRUD
+    private IMensajeriaService mensajeriaService;
+
+    // Inyección de dependencia del servicio que proporciona servicios de CRUD
+    @Autowired
+    public MensajeriaRestController(IMensajeriaService mensajeriaService) {
+        this.mensajeriaService = mensajeriaService;
+    }
+
+    // Metodo que retorna todos los mensajerias
+    @GetMapping("/mensajerias")
+    public List<Mensajeria> getMensajerias(){
+        return mensajeriaService.findAll();
+    }
+
+    // Metodo que guarda un mensajeria pasandolo por el cuerpo de la petición
+    @PostMapping("/mensajerias")
+    public Mensajeria save(@RequestBody Mensajeria mensajeria) {
+        return mensajeriaService.save(mensajeria);
+    }
+
+    // Metodo que elimina un mensajeria pasandolo por el cuerpo de la petición
+    @DeleteMapping("/mensajerias")
+    public void delete(@RequestBody Mensajeria mensajeria){
+        mensajeriaService.delete(mensajeria);
+    }
+
+    // Metodo que actualiza un mensajeria pasandolo por el cuerpo de la petición
+    @PutMapping("/mensajerias")
+    public Mensajeria update(@RequestBody Mensajeria mensajeria){
+        return mensajeriaService.update(mensajeria);
+    }
+
+    // Metodo que retorna un mensajeria por su id pasado por la URL
+    @GetMapping("/mensajerias/{id}")
+    public Mensajeria findById(@PathVariable("id") Long id){
+        return mensajeriaService.findById(id);
+    }
+
+
+}
